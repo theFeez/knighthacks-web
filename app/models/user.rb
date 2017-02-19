2783a308f6d237b73c10e446d8e0e9800da6ba31
Steven Petryk <me@stevenpetryk.com>
2017-02-19 11:06:36 -0500

    Fix blank strings in user availability (#13)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   def shirt_fit
     "#{shirt_fit_sex} #{shirt_fit_size}"
   end
+
+  # HACK figure out why simple_form generates hidden inputs with empty strings.
+  def availability=(list)
+    super list.select(&:presence)
+  end
 end
